d09bd20b08b443e207b0c254e741520c1c31eaf5
Alexis Miguel Hernandez Ovalle <miguelovalle2909@gmail.com>
2024-09-07 10:04:55 -0600

    primer commit intro ts

---
+++ src/temas-ts/01-tipos-bas.ts
@@ -0,0 +1,17 @@
+
+var num1=22;
+let num2='juan';
+const num3=34;
+
+const Sum = num1 + num2 + num3;
+console.log(`La suma es: ${Sum}`);
+
+let nombre:string= 'juan';
+let edad:number = 23;
+let activo:boolean =true;
+
+//edad='dario'
+
+let matricula:number|string;
+matricula=123456;
+matricula="sausanx"
