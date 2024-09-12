7a92829566479f1b3920b1006d0a3e74b985036b
Alexis Miguel Hernandez Ovalle <miguelovalle2909@gmail.com>
2024-09-11 20:58:55 -0600

    "Actividad 2 miercoles 11 de sep"

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import './temas-ts/01-tipos-bas.ts'
-import './temas-ts/02-interfaz-arre.ts'
-import './temas-ts/03-funciones.ts'
+//import './temas-ts/01-tipos-bas.ts'
+//import './temas-ts/02-interfaz-arre.ts'
+//import './temas-ts/03-funciones.ts'
+//import './temas-ts/04-desestructuracion-obj-arre.ts'
+//import './temas-ts/05-desestructuracion.funciones.ts'
+//import './temas-ts/06-import-export.ts'
+import './temas-ts/07-classe.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
